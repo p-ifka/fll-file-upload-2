c9cd3f6e2052f8b07b1d20c1b2541fac9fa0b4a2
watercereal <watercereal11@gmail.com>
2025-09-06 12:14:49 -0400

    add function to htmlformatter for reading html from file

--- app/src/main/java/fll/fileserver/html/HtmlFormatter.java
+++ app/src/main/java/fll/fileserver/html/HtmlFormatter.java
@@ -1,12 +1,56 @@
 package fll.fileserver.html;
 
 import java.util.HashMap;
+import java.io.IOException;
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 public class HtmlFormatter
 {
     private static final char FORMAT_INDICATOR = '~';
 
+    public static String readWholePageFile(String path)
+    /**
+    return the contents of file as String
 
+    @param String path : path to file
+    **/
+    {
+	int bufferSize = 1024;
+	StringBuffer buffer = new StringBuffer(bufferSize);
+	File page;
+	InputStream pageReader;
+	int nextb;
+
+
+	page = new File(path);
+	if(page.exists() && page.canRead()) {
+	    try {
+		pageReader = new FileInputStream(page);
+		for(int i=0;true;i++) {
+		    nextb = pageReader.read();
+		    if(nextb == - 1) {
+			System.out.println("page reader completed after " + i + "bytes");
+			return buffer.toString();
+		    } else {
+			if(i >= bufferSize) {
+			    bufferSize += 1024;
+			    buffer.setLength(bufferSize);
+			}
+			buffer.append((char)nextb);
+		    }
+		}
+	    } catch(IOException e) {
+		System.out.println(String.format("|HtmlFormatter.readWholePageFile| IOException : %s", e.getMessage()));
+		return "";
+	    }
+	}
+
+	return "";
+    }
+
+    
     public static String format(String fmt,
 				HashMap<String, String> args)
     /** format string using named tags surrounded by **/
