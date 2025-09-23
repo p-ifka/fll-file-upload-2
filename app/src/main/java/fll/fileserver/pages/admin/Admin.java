cbcd6e18e57dce1da9be0c49ced4174d44e3c42f
watercereal <watercereal11@gmail.com>
2025-09-22 23:02:23 -0400

    allow creating users with labels and editing existing groups labels on admin page

--- app/src/main/java/fll/fileserver/pages/admin/Admin.java
+++ app/src/main/java/fll/fileserver/pages/admin/Admin.java
@@ -35,16 +35,19 @@ implements HttpHandler
     private final String GROUP_NAME_FORM_INPUT = "group-name"; // 'name' attribute of <input> for the group name in group modification operations
     private final String GROUP_EXPIRY_FORM_INPUT = "new-group-expiry"; // 'name' attribute of <input> for the expiration date of a group in group modification operations
     private final String QUANTITY_FORM_INPUT = "quantity";
+    private final String USER_PASS_FORM_INPUT = "user-pass";
+    private final String USER_LABEL_FORM_INPUT = "user-label";
 
 
-    private Log logInst;
+
+    private Log log;
     private Database dbInst;
     private FileManager fileMgr;
 
 
     public Admin(Log logInstance, Database databaseInstance, FileManager fileManager)
     {
-	logInst = logInstance;
+	log = logInstance;
 	dbInst = databaseInstance;
 	fileMgr = fileManager;
     }
@@ -65,14 +68,14 @@ implements HttpHandler
 
 
 
-	// logInst.info("rq to /admin/add-group recieved");
-	// logInst.info(exchange.getRequestMethod());
+	// log.info("rq to /admin/add-group recieved");
+	// log.info(exchange.getRequestMethod());
 	try {
 	    InputStream reqBody = exchange.getRequestBody();
 
 	    // String t = HttpUtil.postNextParameter(reqBody);
 	    if(HttpUtil.postNextParameter(reqBody).equals(GROUP_NAME_FORM_INPUT)) {
-		// logInst.info((String.format("%s parameter found", GROUP_NAME_FORM_INPUT)));
+		// log.info((String.format("%s parameter found", GROUP_NAME_FORM_INPUT)));
 		newGroupName = HttpUtil.postParameterValue(reqBody);
 		if(newGroupName == null) {
 		    HttpUtil.badRequest(exchange);
@@ -81,7 +84,7 @@ implements HttpHandler
 
 		if(HttpUtil.postNextParameter(reqBody).equals(GROUP_EXPIRY_FORM_INPUT)) {
 		    newGroupExpiryDate = HttpUtil.postParameterValue(reqBody);
-		    logInst.info(newGroupExpiryDate);
+		    log.info(newGroupExpiryDate);
 		    if(newGroupExpiryDate != null) {
 			hasExpiry = true;
 		    }
@@ -89,7 +92,7 @@ implements HttpHandler
 
 		int rc = Database.DB_SUCCESS;
 		if(hasExpiry) {
-		    // logInst.info(String.format("new group expiry: %s", newGroupExpiryDate));
+		    // log.info(String.format("new group expiry: %s", newGroupExpiryDate));
 		    rc = dbInst.createGroup(newGroupName, newGroupExpiryDate);
 		} else {
 		    rc = dbInst.createGroup(newGroupName, Database.DB_EXPIRY_NONE);
@@ -98,19 +101,18 @@ implements HttpHandler
 		if(rc != Database.DB_SUCCESS) {
 		    HttpUtil.badRequest(exchange);
 		}
-		
+
 		// if(HttpUtil.postNextParameter(reqBody) == GROUP_EXPIRT_FORM_INPUT) {
 		    // String newGroupExpiry = HttpUtil.postNextParameter(reqBody);
-		    // logInst.info(String.format("creating new group with expiry: %s", newGroupExpiry));
+		    // log.info(String.format("creating new group with expiry: %s", newGroupExpiry));
 		} else {	// no group name parameter: ignore request
 		HttpUtil.badRequest(exchange);
 		return;
 	    }
+	    HttpUtil.redirectSeeOther(exchange, "/admin");
 
-	    // logInst.info(("returning OK"));
-	    // HttpUtil.OK(exchange);
 	} catch(IOException e) {
-	    logInst.error(String.format("|pages.admin.Admin.handleAddGroup| IOException : %s", e.getMessage()));
+	    log.error(String.format("|pages.admin.Admin.handleAddGroup| IOException : %s", e.getMessage()));
 	    HttpUtil.internalServerErr(exchange);
 	    return;
 	}
@@ -125,7 +127,9 @@ implements HttpHandler
     **/
     {
 	String groupName;
+	String secondArg;
 	String quantityStr;
+
 	int quantity;
 	int rc;
 
@@ -142,32 +146,92 @@ implements HttpHandler
 		return;
 	    }
 
-	    if(HttpUtil.postNextParameter(reqBody).equals(QUANTITY_FORM_INPUT)) {
+	    secondArg = HttpUtil.postNextParameter(reqBody);
+	    if(secondArg.equals(QUANTITY_FORM_INPUT)) {
 		quantityStr = HttpUtil.postParameterValue(reqBody);
 		if(quantityStr == null) { HttpUtil.badRequest(exchange); return; }
 		quantity = Integer.parseInt(quantityStr);
+
+		rc = dbInst.createUsers(groupName, quantity);
+		if(rc == Database.DB_SUCCESS) {
+		    HttpUtil.redirectSeeOther(exchange, "/admin");
+		} else if(rc == Database.DB_ISSUE) {
+		    HttpUtil.badRequest(exchange);
+		} else {
+		    HttpUtil.internalServerErr(exchange);
+		}
+	    } else if(secondArg.equals(USER_LABEL_FORM_INPUT)) {
+		secondArg = HttpUtil.postParameterValue(reqBody);
+		if(secondArg == null) { HttpUtil.badRequest(exchange); return; }
+		rc = dbInst.createLabeledUser(groupName, secondArg);
+		if(rc == Database.DB_SUCCESS) {
+		    HttpUtil.redirectSeeOther(exchange, "/admin");
+		} else if(rc == Database.DB_ISSUE) {
+		    HttpUtil.badRequest(exchange);
+		} else {
+		    HttpUtil.internalServerErr(exchange);
+		}
+
 	    } else {
 		HttpUtil.badRequest(exchange);
 		return;
 	    }
 
-	    
-	    rc = dbInst.createUsers(groupName, quantity);
-	    if(rc == Database.DB_SUCCESS) {
-		HttpUtil.OK(exchange);
-	    } else if(rc == Database.DB_ISSUE) {
-		HttpUtil.badRequest(exchange);
-	    } else {
-		HttpUtil.internalServerErr(exchange);
-	    }
+
+
 
 
 	} catch(IOException e) {
-	    logInst.error(String.format("|pages.admin.Admin.handleAddUsers| IOException : %s", e.getMessage()));
+	    log.error(String.format("|pages.admin.Admin.handleAddUsers| IOException : %s", e.getMessage()));
 	    HttpUtil.internalServerErr(exchange);
 	    return;
 	}
 
+    }
+
+    private void handleSetLabel(HttpExchange exchange)
+    {
+	InputStream reqBody;
+	String pass;
+	String label;
+	int rc;
+
+
+	try {
+	    reqBody = exchange.getRequestBody();
+	    
+	    if(HttpUtil.postNextParameter(reqBody).equals(USER_PASS_FORM_INPUT)) {
+		pass = HttpUtil.postParameterValue(reqBody);
+		if(pass == null) { HttpUtil.badRequest(exchange); return; }
+	    } else {
+		HttpUtil.badRequest(exchange);
+		return;
+	    }
+	    
+	    if(HttpUtil.postNextParameter(reqBody).equals(USER_LABEL_FORM_INPUT)) {
+		label = HttpUtil.postParameterValue(reqBody);
+		if(label == null) { HttpUtil.badRequest(exchange); return; }
+	    } else {
+		HttpUtil.badRequest(exchange);
+		return;
+	    }
+
+	    rc = dbInst.setUserLabel(pass, label);
+	    switch(rc) {
+	    case Database.DB_SUCCESS:
+		HttpUtil.OK(exchange);
+		return;
+	    case Database.DB_ISSUE:
+		HttpUtil.badRequest(exchange);
+		return;
+	    default:
+		HttpUtil.internalServerErr(exchange);
+		return;
+	    }
+	} catch(IOException e) {
+	    log.error(String.format("|pages.admin.Admin.handleSetLabel| IOException : %s", e.getMessage()));
+	    HttpUtil.internalServerErr(exchange);
+	}
     }
 
 
@@ -178,7 +242,7 @@ implements HttpHandler
 		HttpUtil.notFound(exchange);
 		return;
 	    }
-	    
+
 	    Database.SqlQuery userListQuery;
 	    int userTableBufferLen;
 	    StringBuffer userTable;
@@ -187,31 +251,41 @@ implements HttpHandler
 
 	    String page;
 	    HashMap<String,String> pageArgs;
+
+	    String user;
+	    HashMap<String,String> userArgs;
+
 	    Headers responseHeaders;
 	    OutputStream responseBody;
-	    
+
+
 
 	    userListQuery = dbInst.getUsersInGroup(groupID);
 
 	    userTableBufferLen = 2048;
 	    userTable = new StringBuffer(userTableBufferLen);
-	    
-	    while(userListQuery.result.next()) {
-		userTableItem = new HtmlElement("li");
-		userTableItem.setContents(userListQuery.result.getString(1));
 
+	    while(userListQuery.result.next()) {
+		userArgs = new HashMap<String,String>();
+		userArgs.put("user-pass", userListQuery.result.getString(1));
+		userArgs.put("user-label", userListQuery.result.getString(3));
+		userArgs.put("user-pass-form-input", USER_PASS_FORM_INPUT);
+		userArgs.put("user-label-form-input", USER_LABEL_FORM_INPUT);
+		user = HtmlFormatter.format(HtmlFormatter.readWholePageFile("elements/admin-user-data.temp.html"), userArgs);
+
+		i+= user.length();
 		if(i >= userTableBufferLen) {
 		    userTableBufferLen = userTableBufferLen + 2048;
 		    userTable.setLength(userTableBufferLen);
 		}
-		userTable.append(userTableItem.toElementString());
-		i++;
+		userTable.append(user);
 	    }
 	    userListQuery.free();
-	    
+
 	    pageArgs = new HashMap<String,String>();
 	    pageArgs.put("group-name", groupID);
 
+	    pageArgs.put("css", HtmlFormatter.readWholePageFile("pages/theme.css"));
 	    pageArgs.put("user-list", userTable.toString());
 	    page = HtmlFormatter.format(HtmlFormatter.readWholePageFile("pages/admin-group.temp.html"), pageArgs);
 
@@ -226,33 +300,33 @@ implements HttpHandler
 
 		responseBody.close();
 		exchange.close();
-		
-		
+
+
 	    } catch(IOException e) {
-		logInst.error(String.format("|pages.admin.Admin.showGroup| IOException : %s", e.getMessage()));
+		log.error(String.format("|pages.admin.Admin.showGroup| IOException : %s", e.getMessage()));
 		HttpUtil.internalServerErr(exchange);
 		return;
 	    }
-	    
-	    
+
+
 	} catch(SQLException e) {
-	    logInst.error(String.format("|pages.admin.Admin.showGroup| SQLException : %s", e.getMessage()));
+	    log.error(String.format("|pages.admin.Admin.showGroup| SQLException : %s", e.getMessage()));
 	    HttpUtil.internalServerErr(exchange);
 	    return;
 	}
-	
+
     }
 
     @Override
     public void handle(HttpExchange exchange)
     {
-	logInst.info("request to /admin/");
+	log.info("request to /admin/");
 	// URI reqURI = exchange.getRequestURI();
 
 	String URI = exchange.getRequestURI().getPath();
 	String[] URIComponents = URI.split("/");
 	if(exchange.getRequestMethod().equals("POST") && URIComponents.length >= 3) {
-	    logInst.info("POST request recieved");
+	    log.info("POST request recieved");
 	    switch(URIComponents[2]) {
 	    case "add-group":
 		handleAddGroup(exchange);
@@ -260,19 +334,24 @@ implements HttpHandler
 	    case "add-users":
 		handleAddUsers(exchange);
 		return;
+	    case "set-label":
+		handleSetLabel(exchange);
+		return;
+
+		
 	    }
 
 	}
 
 	if(URIComponents.length >= 4 && URIComponents[2].equals("group")) {
-	    logInst.info("showing group " + URIComponents[3]);
+	    log.info("showing group " + URIComponents[3]);
 	    showGroup(exchange, URIComponents[3]);
 	    return;
 	}
 
 
 
-	logInst.info("displaying main page");
+	log.info("displaying main page");
 	String groupTable = "";
 
 
@@ -285,6 +364,7 @@ implements HttpHandler
 		HtmlElement row = new HtmlElement("div");
 		HashMap<String, String> divArgs = new HashMap<String, String>();
 
+
 		divArgs.put("group-name", gtGroupName);
 		divArgs.put("group-link", String.format("/admin/group/%s", gtGroupName));
 		divArgs.put("group-expiry", groups.getString(2));
@@ -292,6 +372,7 @@ implements HttpHandler
 		divArgs.put("group-user-quantity", String.format("%d", dbInst.getUserCount(gtGroupName)));
 		divArgs.put("group-name-form-input", GROUP_NAME_FORM_INPUT);
 		divArgs.put("quantity-form-input", QUANTITY_FORM_INPUT);
+		divArgs.put("user-label-form-input",  USER_LABEL_FORM_INPUT);
 
 		row.setContents(HtmlFormatter.format(HtmlFormatter.readWholePageFile("elements/admin-group-data.temp.html"), divArgs));
 
@@ -301,12 +382,13 @@ implements HttpHandler
 	    getGroupsQuery.free();
 
 	} catch(SQLException e) {
-	    logInst.error(String.format("sql error: %s", e.getMessage()));
+	    log.error(String.format("sql error: %s", e.getMessage()));
 	    System.exit(1);	// TODO: internal server error page here
 	}
 
 	HashMap<String, String> args = new HashMap<String,String>();
 
+	args.put("css", HtmlFormatter.readWholePageFile("pages/theme.css"));
 	args.put("group-name-form-input", GROUP_NAME_FORM_INPUT);
 	args.put("group-expiry-input", GROUP_EXPIRY_FORM_INPUT);
 	args.put("table-contents", groupTable);
@@ -325,7 +407,7 @@ implements HttpHandler
 	    outStream.close();
 	    exchange.close();
 	} catch(IOException e) {
-	    logInst.error(String.format("io exception: %s", e.getMessage()));
+	    log.error(String.format("io exception: %s", e.getMessage()));
 	    System.exit(1);
 	}
 
